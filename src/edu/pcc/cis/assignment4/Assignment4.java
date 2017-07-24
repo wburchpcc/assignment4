26aa868ed64d4576230a78fe293f7b76921d4cae
Will Burchard <w.burchard@gmail.com>
2017-07-24 15:59:36 -0700

    Added Assignment Details

--- src/edu/pcc/cis/assignment4/Assignment4.java
+++ src/edu/pcc/cis/assignment4/Assignment4.java
@@ -4,4 +4,8 @@ package edu.pcc.cis.assignment4;
  * Created by Will Burchard on 7/24/2017.
  */
 public class Assignment4 {
+    public static void main (String[] args) {
+        System.out.println("This is Assignment 4.");
+        System.out.println("In Assignment 4, we set up version control using Git and GitHub.");
+    }
 }
