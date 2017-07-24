b07b0d54381a9dc9f303dae0b3e8016a97b5394b
Will Burchard <w.burchard@gmail.com>
2017-07-24 16:07:53 -0700

    Added Date to Print Out

--- src/edu/pcc/cis/assignment4/Assignment4.java
+++ src/edu/pcc/cis/assignment4/Assignment4.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.assignment4;
 
+import java.util.Date;
+
 /**
  * Created by Will Burchard on 7/24/2017.
  */
@@ -7,5 +9,7 @@ public class Assignment4 {
     public static void main (String[] args) {
         System.out.println("This is Assignment 4.");
         System.out.println("In Assignment 4, we set up version control using Git and GitHub.");
+
+        System.out.println("Submitted on " + new Date());
     }
 }
